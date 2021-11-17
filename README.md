9f42a377a1bc120334ef2cd55be6698f7628a45a
Chris Tomkins <chris.tomkins@tigera.io>
2021-11-17 11:04:08 +0000

    Dockerised. Still WIP.

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # caliconodedockerstats
+A simple Golang tool to pull calico-node's pull_count from Docker Hub and present it via a Prometheus target.
+## Documentation:
+This documentation is a work-in-progress and should not yet be considered complete. Hopefully it is still of some use in this state.
+## Building:
+```
+git pull && docker build -t caliconodedockerstats-`git log --pretty=format:'%h' -n 1` .
+```
+## Testing Using A Local Kubeconfig:
+```
+docker image ls
+export CALICONODEDOCKERSTATS_ATTR_NAME="pull_count" && export CALICONODEDOCKERSTATS_TARGET_NAME="https://hub.docker.com/v2/repositories/calico/node/" && docker run --env-file ./env.list caliconodedockerstats-`git log --pretty=format:'%h' -n 1`
+```
--- main.go
+++ main.go
@@ -1,32 +1,39 @@
 package main
 
 import (
-   "io"
-   "fmt"
-   "github.com/tidwall/gjson"
-   "log"
-   "net/http"
-//   "github.com/prometheus/client_golang/prometheus/promhttp"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/tidwall/gjson"
+	//	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func main() {
-//   http.Handle("/metrics", promhttp.Handler())
-//   http.ListenAndServe(":2112", nil)
-   resp, err := http.Get("https://hub.docker.com/v2/repositories/calico/node/")
-   if err != nil {
-      log.Fatalln(err)
-   }
+	// Grab from the API target details from the env
+	the_attr := os.Getenv("CALICONODEDOCKERSTATS_ATTR_NAME")
+	the_target := os.Getenv("CALICONODEDOCKERSTATS_TARGET_NAME")
 
-//We Read the response body on the line below.
-   body, err := io.ReadAll(resp.Body)
-   if err != nil {
-      log.Fatalln(err)
-   }
+	// Get the response from the API and log any error
+	resp, err := http.Get(the_target)
+	if err != nil {
+		log.Fatalln(err)
+	}
 
-//Convert the body to type string
-   fmt.Println(string(body))
-   sb := string(body)
-   theresult := gjson.Get(sb,"pull_count")
-   log.Printf(theresult.Str)
-   log.Printf(sb)
+	// Read the response body and log any error
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// Convert the body and target attribute to a string and print it
+	string_body := string(body)
+	the_result := gjson.Get(string_body, the_attr)
+	fmt.Println(string(body))
+	fmt.Println(the_result.Str)
+
+	//	http.Handle("/metrics", promhttp.Handler())
+	//	http.ListenAndServe(":2112", nil)
 }
